45014a4aa30df8af62715f9cc7ea79c9bc49dbdb
takumatt <takuma.matt@gmail.com>
2024-03-08 16:29:20 +0900

    WIP: Implement substitution build func

--- src/xcstring_substitution_builder.rs
+++ src/xcstring_substitution_builder.rs
@@ -1,5 +1,6 @@
 use icu_messageformat_parser::{self, AstElement};
-use crate::xcstrings;
+use linked_hash_map::LinkedHashMap;
+use crate::xcstrings::{self, StringUnit, Substitution};
 
 pub struct XCStringSubstitutionBuilder {
 
@@ -13,11 +14,27 @@ impl XCStringSubstitutionBuilder {
     }
 
     pub fn build(&self, plurals: Vec<AstElement>) -> xcstrings::Substitution {
+
         let mut substitution = xcstrings::Substitution {
             arg_num: todo!(),
             format_specifier: todo!(),
-            variations: todo!(),
+            variations: todo!()
         };
-        substitution  
+
+        let variations: LinkedHashMap<String, StringUnit> = LinkedHashMap::from_iter(plurals.iter().map(|plural| {
+            match plural {
+                AstElement::Plural { value, plural_type, span, offset, options } => {
+                    let value = value.to_string();
+                    (value, StringUnit {
+                        localization_state: todo!(),
+                        value,
+                    })
+                },
+                _ => panic!("Invalid plural element")
+            }
+        }));
+
+        substitution
+
     }
 }
