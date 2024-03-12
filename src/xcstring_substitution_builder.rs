01b6ea2a719ef8b8dd17f953229f6a92f2219f81
takumatt <takuma.matt@gmail.com>
2024-03-13 00:34:27 +0900

    Support =0 and =1 notation

--- src/xcstring_substitution_builder.rs
+++ src/xcstring_substitution_builder.rs
@@ -17,6 +17,7 @@ impl XCStringSubstitutionBuilder {
 
     pub fn build(&self, plurals: Vec<AstElement>) -> LinkedHashMap<String, Substitution> {
         let mut formatter = XCStringFormatter::new(FormatterMode::Plural);
+        // TODO: Verify key uniqueness, contains only valid keys
         plurals.iter().enumerate().fold(LinkedHashMap::new(), |mut map, (index, plural)| {
             let pair = match plural {
                 AstElement::Plural { value, plural_type, span, offset, options } => {
@@ -32,7 +33,7 @@ impl XCStringSubstitutionBuilder {
                                     .collect::<Vec<String>>()
                                     .join("");
                                 println!("formatted_strings: {:?}", formatted_strings);
-                                map.insert(key.to_string(), VariationValue {                                    
+                                map.insert(KeyFormat::from_string(&key.to_string()).to_string(), VariationValue {                                    
                                     string_unit: StringUnit {
                                         localization_state: xcstrings::LocalizationState::Translated,
                                         value: formatted_strings
@@ -48,5 +49,40 @@ impl XCStringSubstitutionBuilder {
             map.insert(pair.0.clone(), pair.1);
             map
         })
+    }
+}
+
+enum KeyFormat {
+    Zero,
+    One,
+    Other
+}
+
+impl KeyFormat {
+    fn from_string(key: &String) -> KeyFormat {
+        match key.as_str() {
+            "zero" => KeyFormat::Zero,
+            "one" => KeyFormat::One,
+            "two" => panic!("String Catalog doesn't support custom keys starting with 'two'"),
+            "few" => panic!("String Catalog doesn't support custom keys starting with 'few'"),
+            "many" => panic!("String Catalog doesn't support custom keys starting with 'many'"),
+            "other" => KeyFormat::Other,
+            "=0" => KeyFormat::Zero, // "zero" is an alias for "=0"
+            "=1" => KeyFormat::One, // "one" is an alias for "=1"
+            _ => {
+                if key.as_str().starts_with("=") {
+                    panic!("String Catalog doesn't support custom keys starting with '=' except for zero and one")
+                }
+                panic!("Unexpected key format")
+            }
+        }
+    }
+
+    fn to_string(&self) -> String {
+        match self {
+            KeyFormat::Zero => "zero".to_string(),
+            KeyFormat::One => "one".to_string(),
+            KeyFormat::Other => "other".to_string()
+        }
     }
 }
